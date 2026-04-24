44800a9d0e2dc3ca8a4d8eac4fd0d054016f1eeb
allaindm <allaindemoupu@mail.ru>
2026-04-24 18:04:10 +0300

    Добавлен 8-й ход

--- map-script.js
+++ map-script.js
@@ -33,7 +33,7 @@ img.addEventListener("click",() => {
 // })
 
 polit.addEventListener("click",() => {
-    img.src="maps/07.png"
+    img.src="maps/08.png"
 })
 
 resources.addEventListener("click",() => {
