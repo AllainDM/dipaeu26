e658e6b7bbe7793582a2aa3dc0dce2111e34eb25
Bogdan Bescherepov <bogdanrus@list.ru>
2026-07-08 14:10:14 +0300

    Добавлена карта Чумы с кнопкой и дисклеймером

--- map-script.js
+++ map-script.js
@@ -15,6 +15,7 @@ const religion = document.getElementById("relig");
 const tts = document.getElementById("tts");
 const hre = document.getElementById("hre");
 const chuma = document.getElementById("chuma");
+const disclaimer = document.getElementById("chuma-disclaimer");
 const goroda = document.getElementById("goroda");
 
 img.addEventListener("click",() => {
@@ -36,40 +37,53 @@ img.addEventListener("click",() => {
 //     img.src="maps/igroki.png"
 // })
 
+function hideDisclaimer() {
+    disclaimer.style.display = "none";
+}
+
 polit.addEventListener("click",() => {
     img.src="maps/polit/" + LATEST_POLIT_MAP + ".png"
+    hideDisclaimer();
 })
 
 resources.addEventListener("click",() => {
     img.src="maps/resources.png"
+    hideDisclaimer();
 })
 
 climate.addEventListener("click",() => {
     img.src="maps/climate.png"
+    hideDisclaimer();
 })
 
 regions.addEventListener("click",() => {
     img.src="maps/Karta_regionov.png"
+    hideDisclaimer();
 })
 
 ethic.addEventListener("click",() => {
     img.src="maps/ethic.png"
+    hideDisclaimer();
 })
 
 religion.addEventListener("click",() => {
     img.src="maps/religion.png"
+    hideDisclaimer();
 })
 
 tts.addEventListener("click",() => {
     img.src="maps/Karta_TTs.png"
+    hideDisclaimer();
 });
 
 hre.addEventListener("click",() => {
     img.src="maps/Karta_SRI.png"
+    hideDisclaimer();
 });
 
 chuma.addEventListener("click",() => {
     img.src="maps/chuma.png"
+    disclaimer.style.display = "block";
 });
 
 // goroda.addEventListener("click",() => {
